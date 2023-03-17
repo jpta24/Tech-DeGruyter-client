d95e6acb3dfbdbc3bb50c274a0ff4702d4f1bdd1
Jpta24 <jeanpierretorres@gmail.com>
2023-03-17 17:42:42 +0100

    Navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <ul>
+        <li><a href="#">Impressum</a></li>
+        <li>|</li>
+        <li><a href="#">Cookies</a></li>
+        <li>|</li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
   )
 }
 
